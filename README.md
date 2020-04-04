2dfc35ab47e46f8bd934c887babf1af3ea9c9602
mpomykalski <pomykalskimateusz@gmail.com>
2020-04-04 15:12:42 +0200

    Initial commit with basic env setup roles

---
+++ README.md
@@ -0,0 +1,2 @@
+Prompt for password:
+We can use the -K or --ask-become-pass flag to tell Ansible to ask for the user password
---
+++ install_ansible.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get upgrade -y
+
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install ansible -y
+
+sudo apt-get install python -y
